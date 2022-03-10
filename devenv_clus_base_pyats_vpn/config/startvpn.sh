06e0ad16fb95a9e0b8b6c029af732e6c37bfb743
root <admin@example.com>
2022-03-10 09:04:29 -0500

    added pyats base image

---
+++ devenv_clus_base_pyats_vpn/config/startvpn.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+: "${VPN_SERVER:?Variable unset or empty}"
+: "${VPN_USERNAME:?Variable unset or empty}"
+: "${VPN_PASSWORD:?Variable unset or empty}"
+
+run() {
+  echo "$VPN_PASSWORD" | sudo openconnect "$VPN_SERVER" --passwd-on-stdin -u "$VPN_USERNAME" --no-dtls --verbose --timestamp >>/var/log/openconnect/openconnect.log 2>&1
+}
+
+until (run); do
+  echo "openconnect crashed with exit code $? - respawning..." >&2
+  sleep 5
+done
